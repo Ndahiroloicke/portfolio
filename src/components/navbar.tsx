7a1b7294ccc1716c10f0274b787f90247258fb72
Ndahiroloicke <ndahiroloicke@gmail.com>
2024-11-10 13:52:11 +0200

    development

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function Navbar() {
 
-  const [activeItem, setActiveItem] = useState(''); // Store the clicked item's name
+  const [activeItem, setActiveItem] = useState(''); 
 
   return (
     <>
