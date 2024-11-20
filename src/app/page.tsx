2371a650220b26a50e85439f74cccd82ff0b2f76
Ndahiroloicke <ndahiroloicke@gmail.com>
2024-11-20 20:43:33 +0200

    development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Home from "@/components/home";
 import LastICon from "@/components/lasticons";
 import Navbar from "@/components/navbar";
-
+// this is one of the comments
 export default function Page() {
   return (
     <>
