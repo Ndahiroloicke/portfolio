398694596e7597999227f42b3c310f7c85e51ceb
Ndahiroloicke <ndahiroloicke@gmail.com>
2024-12-15 22:34:26 +0200

    Development

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,6 @@ export default function Navbar() {
   const pathname = usePathname();
   const router = useRouter();
   const [activeItem, setActiveItem] = useState(pathname);
-  // Development in progress 
 
   useEffect(() => {
     setActiveItem(pathname);
